4a5cad26feea6e2ed5b6cbc07700132cf904621b
Exavier Lattimore <exavierexe@gmail.com>
2025-01-05 11:03:13 +1300

    a

--- components/ui/brandlogo.tsx
+++ components/ui/brandlogo.tsx
@@ -5,7 +5,7 @@ export function BrandLogo() {
         
         <span className="flex items-center gap-2 font-semibold flex-shrink-0 text-lg">
             <Image alt="" src="/favicon.ico" width="100" height="100" className="size-8"/>
-            <Image alt="" src="/0.png" width="100" height="100" className="size-8"/>
+            
             <span>Exavier&apos;s School</span>
         </span>
         
