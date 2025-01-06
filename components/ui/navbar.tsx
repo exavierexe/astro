c7df592359299bd33f4317148a28a3b3cf409988
Exavier Lattimore <exavierexe@gmail.com>
2025-01-06 21:07:52 +1300

    auth

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,11 +1,13 @@
 import { BrandLogo } from "./brandlogo"
 import Link from "next/link" 
+import { SignedIn, SignedOut, SignInButton  } from "@clerk/nextjs"
 
 
 
 
 export function NavBar() {
-    return <header className="flex py-6 shadow-xl fixed top-0 w-full z-10 bg-background/95">
+    return 
+    <header className="flex py-6 shadow-xl fixed top-0 w-full z-10 bg-background/95">
         <nav className="flex items-center gap-10 container font-semibold">
             <Link href="/" className="mr-auto">
                 <BrandLogo />
@@ -25,6 +27,14 @@ export function NavBar() {
             <Link className="text-lg" href="/library">   
                 Library
             </Link>
+            <span className="text-lg">
+            <SignedIn>
+                <Link href="">Space</Link>
+            </SignedIn>
+            <SignedOut>
+                <SignInButton>Login</SignInButton>
+            </SignedOut>
+            </span>
 
         </nav>
     </header>
