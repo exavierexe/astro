1bcf3fbbf06384bc864559dd8a5b805f117172a0
Exavier Lattimore <exavierexe@gmail.com>
2025-01-10 11:23:15 +1300

    pics

--- components/ui/heroimage.tsx
+++ components/ui/heroimage.tsx
@@ -1,15 +1,6 @@
 "use client";
-import { Card, CardContent } from "./card";
-import { Button } from "./button";
-import Image from "next/image";
-import globe from "../../public/globe.svg";
-import tarot1 from "../../public/tarot/tarot1.png"
+
+import heroimage from "../../public/visuals/heroimage.jpg"
 
 
-export function HeroImage() {
-    return (
-        <div className="relative flex items-center justify-center min-h-screen p-8">
-            <Image alt="" src={tarot1} width="1920" height="1080" className="object-cover"/>
-        </div>
-    )
-}
+export const hero = heroimage;
