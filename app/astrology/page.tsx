061d627b0c1d3defad85d2f0cffb4523ab1f99f5
Exavier Lattimore <exavierexe@gmail.com>
2025-01-13 07:08:24 +1300

    bg

--- app/astrology/page.tsx
+++ app/astrology/page.tsx
@@ -15,7 +15,7 @@ export default function Astrology() {
 
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gradient-to-t from-gray-900 to-zinc-700">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         
        
--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gradient-to-t from-gray-900 to-zinc-600">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] ">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
        
         <section className="min-h-screen flex items-center justify-center text-center text-balance flex-col gap-8 px-9">
